36e90ffc5ff617f38286e01e7f72c5d5e7a1057d
muhammadawa242 <muhammadawa242@gmail.com>
2025-05-25 23:56:47 +0500

    update resume link

--- src/pages/resume/Resume.js
+++ src/pages/resume/Resume.js
@@ -6,7 +6,7 @@ const Resume = ({ brand }) => {
   return (
     <section className="resume container">
       <PageHeader title="Resume" description="Uncover my expertise" />
-            <a target="_blank" href="https://docs.google.com/document/d/1wP6wDUTpeZZLWV2prCfHgrvzzPgsOJwS/edit?usp=sharing&ouid=110563412392633040581&rtpof=true&sd=true">
+            <a target="_blank" href="https://docs.google.com/document/d/1OhWW6ZFwG3tUn-ctitB0TFT5t6Z6kYFaZyCV48o2MT8/edit?usp=sharing&ouid=110563412392633040581&rtpof=true&sd=true">
                 <motion.button
                 className="btn"
                 // Apply scale animation on hover
